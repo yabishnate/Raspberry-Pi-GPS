c6372f0e1c11311cd2a4f1de28924055a87d665c
yabishnate <80362305+yabishnate@users.noreply.github.com>
2021-03-10 10:14:27 -0500

    Update gps.py

--- gps.py
+++ gps.py
@@ -1,11 +1,12 @@
+#Impoet serial port, import string module, import pynmea2 library
 import serial
-import time
 import string
 import pynmea2
 
+
 while True:
         port="\dev\ttyAMA0"
-        ser=serial.Serialport, baudrate=9600, timeout=0.5)
+        ser=serial.Serialport, 9600, timeout=0.5)
         dataout = pynmea2.NMEAStreamReader()
         newdata=ser.readline()
         
